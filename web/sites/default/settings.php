525871c261512f07d406b4ec5672a44e313d512d
Aleksi Johansson <aleksi@johansson.plus>
2022-01-08 23:21:39 +0200

    Setup DDEV.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -93,3 +93,9 @@ if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 if (isset($_ENV['SILTA_CLUSTER']) && file_exists($app_root . '/' . $site_path . '/settings.silta.php')) {
   include $app_root . '/' . $site_path . '/settings.silta.php';
 }
+
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+  require $ddev_settings;
+}
